4fabe87872b5871a12a6cea2f68199e519b7980a
Renier Morales <renierm@us.ibm.com>
2019-11-30 10:50:12 -0600

    Add new config to kubeconfig path before renaming the context

--- iksconfig.sh
+++ iksconfig.sh
@@ -3,4 +3,5 @@
 ibmcloud ks cluster config "${1}"
 cluster_id=$(ibmcloud ks cluster-get "${1}" --json -s | grep '"id"' | cut -d '"' -f 4)
 cluster_name=${2:-$1}
+source kubectx > /dev/null
 kubectl config rename-context "${1}"/${cluster_id} "${cluster_name}"
