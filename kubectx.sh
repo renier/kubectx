3578459984b5bbbed4e681ddfca3cfd050cd7cb1
Renier Morales <renierm@us.ibm.com>
2020-02-26 09:18:47 -0600

    Print a better list of contexts working around IBM Cloud long names

--- kubectx.sh
+++ kubectx.sh
@@ -26,5 +26,14 @@ fi
 
 # show context map at the end
 if [ $# -eq 0 ] || [ $# -eq 2 ]; then
-    kubectl config get-contexts
+    FORMAT="%-1s %-30s %-20s %-15s"
+    printf "$FORMAT\n" " " "NAME" "USER" "NAMESPACE"
+    kubectl config get-contexts --no-headers | sed -E 's|/[a-zA-Z0-9]+||g' | sed -e 's|^ |-|g' | while IFS='$\n' read -r line; do
+        current=$(echo "$line" | awk '{print $1}')
+        [ "$current" != "*" ] && current=" "
+        cluster=$(echo "$line" | awk '{print $2}')
+        user=$(echo "$line" | awk '{print $4}')
+        namespace=$(echo "$line" | awk '{print $5}')
+        printf "$FORMAT\n" "$current" "$cluster" "$user" "$namespace"
+    done
 fi
