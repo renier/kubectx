682e64968ffb1bc3a36aef523eb4d3e2d8b2ebb3
Renier Morales <renierm@us.ibm.com>
2020-03-19 14:32:43 -0500

    Update for latest ibmcloud cli

--- iksconfig.sh
+++ iksconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 cluster_name="$1"
-ibmcloud ks cluster config "$cluster_name"
-cluster_id=$(ibmcloud ks cluster-get "$cluster_name" --json -s | grep '"id"' | cut -d '"' -f 4)
+ibmcloud ks cluster config --cluster "$cluster_name"
+cluster_id=$(ibmcloud ks cluster get --cluster "$cluster_name" --json -s | grep '"id"' | cut -d '"' -f 4)
 set --
 source kubectx > /dev/null
 set -eo pipefail
