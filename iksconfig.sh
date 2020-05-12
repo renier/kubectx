6962439de32231a517b72dd9b2fbfd7731cc491a
Renier Morales <renierm@us.ibm.com>
2020-05-11 21:10:53 -0500

    Delete target context before the rename

--- iksconfig.sh
+++ iksconfig.sh
@@ -5,5 +5,6 @@ ibmcloud ks cluster config --cluster "$cluster_name"
 cluster_id=$(ibmcloud ks cluster get --cluster "$cluster_name" --json -s | grep '"id"' | cut -d '"' -f 4)
 set --
 source kubectx > /dev/null
+kubectl config delete-context "${cluster_name}" || true
 set -eo pipefail
 kubectl config rename-context "${cluster_name}"/${cluster_id} "${cluster_name}" && kubectx "${cluster_name}"
