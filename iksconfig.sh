3aea6d2dff8494df724bfecb6aa0eab47f868c43
Renier Morales <renierm@us.ibm.com>
2020-05-15 10:23:06 -0500

    Remove lingering wrongly named context

--- iksconfig.sh
+++ iksconfig.sh
@@ -5,6 +5,8 @@ ibmcloud ks cluster config --cluster "$cluster_name"
 cluster_id=$(ibmcloud ks cluster get --cluster "$cluster_name" --json -s | grep '"id"' | cut -d '"' -f 4)
 set --
 source kubectx > /dev/null
-kubectl config delete-context "${cluster_name}" || true
+kubectl config delete-context "${cluster_name}" > /dev/null 2>&1 || true
 set -eo pipefail
 kubectl config rename-context "${cluster_name}"/${cluster_id} "${cluster_name}" && kubectx "${cluster_name}"
+set --
+kubectl config delete-context "${cluster_name}"/${cluster_id} > /dev/null 2>&1 || true
