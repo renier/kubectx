3458dc00fa7f6715e067a82305ef303ffe9ae286
Renier Morales <renierm@us.ibm.com>
2020-06-16 15:45:16 -0500

    Remember original namespace if context already existed when installing

--- iksconfig.sh
+++ iksconfig.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+orig_ns=$(kubectl config get-contexts --no-headers | grep "${1} " | grep -oE '[^ ]+$')
 set -eo pipefail
 cluster_name="$1"
 ibmcloud ks cluster config --cluster "$cluster_name"
@@ -8,4 +9,6 @@ kubectl config delete-context "${cluster_name}" > /dev/null 2>&1 || true
 set -eo pipefail
 kubectl config rename-context "${cluster_name}"/${cluster_id} "${cluster_name}"
 kubectl config use-context "$cluster_name"
-kubectl config set-context --current --namespace=default
+namespace=default
+[ -n "$orig_ns" ] && namespace="$orig_ns"
+kubectl config set-context --current --namespace="$namespace"
